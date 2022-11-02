efb7e16b30a23a2922b88d4e054467e2b946ae31
Ramiro <ramifazio@gmail.com>
2022-11-02 19:43:56 -0300

    Sugerencias!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,55 +1,37 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import React from "react";
 import CartWidget from "./CartWidget";
-import logo from '../components/Assets/Imagenes/logo.png';
+import logo from "../components/Assets/Imagenes/logo.png";
 export default function Navbar1({ conBoton, color, funcionLogin }) {
+  //* "conBoton" y "funcionLogin" no los vas a poder usar a menos que los mandes por props desde App.js
+  console.log(color);
 
+  return (
+    <div style={{ backgroundColor: color }}>
+      <img src={logo} className="App-logo" alt="logo" width={80} />
 
-    console.log(color);
+      <Navbar expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="Inicio">Inicio</Navbar.Brand>
+          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }} navbarScroll>
+            <Nav.Link href="Maquillaje">Maquillaje</Nav.Link>
+            <Nav.Link href="Skincare">Skincare</Nav.Link>
+            <Nav.Link href="Beauty tools">Beauty tools</Nav.Link>
 
-    return (
-        <div style={{ backgroundColor: color }}>
-
-            <img src={logo} className="App-logo" alt="logo" width={80} />
-
-
-
-            <Navbar expand="lg">
-                <Container fluid>
-                    <Navbar.Brand href="Inicio">Inicio</Navbar.Brand>
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
-                        navbarScroll
-                    >
-                        <Nav.Link href="Maquillaje">Maquillaje</Nav.Link>
-                        <Nav.Link href="Skincare">Skincare</Nav.Link>
-                        <Nav.Link href="Beauty tools">Beauty tools</Nav.Link>
-
-                        <CartWidget />
-                    </Nav>
-                    <Form className="d-flex">
-                        <Form.Control
-                            type="search"
-                            placeholder="Search"
-                            className="me-2"
-                            aria-label="Search"
-                        />
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
-
-                </Container>
-            </Navbar>
-
-
-
-        </div >
-
-    );
+            <CartWidget />
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
